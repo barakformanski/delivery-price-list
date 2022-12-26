e6b26cfdf65604d33f54ad715bf933c4d847b6d0
barakformanski <barakformanski@gmail.com>
2022-12-26 13:59:04 +0200

    cahnge the switch-case to an object to represent images names, and move all of this to seperate file

---
+++ src/pages/deliveryType/utils/chooseSvgObject.jsx
@@ -0,0 +1,19 @@
+import store_delivery from "../../../assets/images/store_delivery.svg";
+import small_move from "../../../assets/images/small_move.svg";
+import storage_move from "../../../assets/images/storage_move.svg";
+import junk_removal from "../../../assets/images/junk_removal.svg";
+import craigslist_Pickup from "../../../assets/images/craigslist_Pickup.svg";
+import donation_pickup from "../../../assets/images/donation_pickup.svg";
+import other from "../../../assets/images/other.svg";
+
+const chooseSvgObject = {
+  store_delivery,
+  small_move,
+  storage_move,
+  junk_removal,
+  craigslist_Pickup,
+  donation_pickup,
+  other,
+};
+
+export default chooseSvgObject;
