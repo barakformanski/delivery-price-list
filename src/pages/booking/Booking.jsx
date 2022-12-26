488d21655ab3ee51b4216f7e9fd240f4b1194fba
barakformanski <barakformanski@gmail.com>
2022-12-26 14:13:55 +0200

    move mockData deliveryOptions to sperate file

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -3,11 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import ArrowUp from "../../assets/images/ArrowUp.svg";
 import ArrowDown from "../../assets/images/ArrowDown.svg";
-import PickUp1 from "../../assets/images/PickUp1.svg";
-import PickUp2 from "../../assets/images/PickUp2.svg";
-import Van2 from "../../assets/images/Van2.svg";
-import VanXl from "../../assets/images/VanXl.svg";
-
+import deliveryOptions from "./assets/mockDataDeliveryOptions";
 import ChangeDeliveryComponent from "../../components/changeDeliveryComponent/CahngeDeliveryComponent";
 import "./Booking.css";
 import { calculatePrice } from "./calculatePrice";
@@ -25,36 +21,6 @@ const Booking = ({
 }) => {
   const navigate = useNavigate();
 
-  const deliveryOptions = [
-    {
-      title: "Pickup 1 Lugger",
-      description: "Single item and small loads",
-      img: PickUp1,
-      pricePerKm: 0.01,
-      pricePerMin: 0.01,
-    },
-    {
-      title: "Pickup 2 Lugger",
-      description: "Single item and small loads",
-      img: PickUp2,
-      pricePerKm: 0.02,
-      pricePerMin: 0.02,
-    },
-    {
-      title: "Van 2 Lugger",
-      description: "Room full of stuff",
-      img: Van2,
-      pricePerKm: 0.03,
-      pricePerMin: 0.03,
-    },
-    {
-      title: "XL 2 Luggers",
-      description: "Studio and 1 bedroom apts",
-      img: VanXl,
-      pricePerKm: 0.04,
-      pricePerMin: 0.04,
-    },
-  ];
   const [modal, setModal] = useState(false);
   const [errorModal, setErrorModal] = useState(false);
   const [submitModal, setsubmitModal] = useState(false);
